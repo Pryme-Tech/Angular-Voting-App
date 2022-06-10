5f861893166ce780b38f1a16ba9e0117a205d4a9
latif83 <latifm8360@gmail.com>
2022-06-10 08:19:01 +0000

    .

--- getvotes.js
+++ getvotes.js
@@ -16,7 +16,10 @@ const pool = new Pool({
   host: 'ec2-52-204-195-41.compute-1.amazonaws.com',
   database: 'ddu7f9ipiuu4mu',
   password: 'd4e30215109ddad350c229cc7676dbef2c242c2038d769ed94d9dc6e88ff57b1',
-  port: 5432
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+ }
 })
 
 router.get('/',(request,response)=>{
