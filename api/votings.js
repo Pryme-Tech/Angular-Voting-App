07d3c4506bcde98540520f6f81eabf7440a7308d
latif83 <latifm8360@gmail.com>
2022-06-27 23:53:21 +0000

    .

--- api/votings.js
+++ api/votings.js
@@ -23,7 +23,7 @@ router.get('/:user',async (req,res)=>{
 
 })
 
-
+router.use(cors())
 
 router.post('/add',async(req,res)=>{
 
@@ -82,20 +82,21 @@ router.post('/add',async(req,res)=>{
 })
 
 router.get('/',async(req,res)=>{
+	res.status(200).json('heloooo')
 
-	try{
-		let Ongoing = await voting.findAll()
+	// try{
+	// 	let Ongoing = await voting.findAll()
 
-		if(Ongoing.length < 1){
-			return res.status(404).json("No Ongoing Votes at the moment")
-		}
+	// 	if(Ongoing.length < 1){
+	// 		return res.status(404).json("No Ongoing Votes at the moment")
+	// 	}
 
-		res.status(200).json(Ongoing)
-	}
-	catch (err){
-		console.log("err")
-		res.json.status(404).("An Error Occured")
-	}
+	// 	res.status(200).json(Ongoing)
+	// }
+	// catch (err){
+	// 	console.log("err")
+	// 	res.json.status(404).("An Error Occured")
+	// }
 })
 
 module.exports = router
