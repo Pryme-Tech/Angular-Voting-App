c54b59a1bfdb08b556c5bba880667f2118462089
latif83 <latifm8360@gmail.com>
2022-07-08 16:55:00 +0000

    .

--- src/app/Administrator/voters/voters.component.ts
+++ src/app/Administrator/voters/voters.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 
+import {FormBuilder,FormControl,FormGroup} from '@angular/forms';
+
 import routes from '../../../assets/routes/routes.json';
 
 @Component({
@@ -11,6 +13,34 @@ import routes from '../../../assets/routes/routes.json';
 export class VotersComponent implements OnInit {
 
   port = routes.host
+
+  votingName = localStorage.getItem('votingname')
+
+  successMessage = ''
+  votingId = ''
+
+  voters = new FormGroup({
+    "votersname" : new FormControl(''),
+    "votingname" : new FormControl(this.votingName)
+  })
+
+  votersOnsubmit(){
+
+    // console.log(this.voters.getRawValue())
+    this.http.post(`${this.port}voters/register`,this.voters.getRawValue()).subscribe(
+      res=>{
+        console.log(res)
+        let result = JSON.parse(JSON.stringify(res))
+
+        this.successMessage = result.msg
+
+        this.votingId = result.index_no
+
+      },
+      err=>{
+
+      })
+  }
 
   constructor(private http: HttpClient) {
 
