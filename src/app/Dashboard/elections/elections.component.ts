a93bc768681493f5dcb378c5dc71b0b78ac9e966
latif83 <latifm8360@gmail.com>
2022-11-23 17:57:23 +0000

    fixed height issue

--- src/app/Dashboard/elections/elections.component.ts
+++ src/app/Dashboard/elections/elections.component.ts
@@ -209,6 +209,13 @@ export class ElectionsComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      vh = window.innerHeight * 0.01;
+      // console.log(vh)
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
   }
 
 }
